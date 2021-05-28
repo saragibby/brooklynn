a6afbe6b79d840b71488e052d0dc80ac9d742d10
Sara Gibbons <saragibby@gmail.com>
2021-05-28 12:53:58 -0400

    update map

--- src/components/map.js
+++ src/components/map.js
@@ -1,36 +1,38 @@
-import React, { useRef, useEffect, useState } from "react"
-import mapboxgl from "mapbox-gl"
-import "mapbox-gl/dist/mapbox-gl.css"
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-const MAPBOX_TOKEN = "pk.eyJ1IjoiY29tcHJlbmR1cyIsImEiOiJja3A3cGFjYjgwMjR4MnZwZXR3dmtzejRsIn0.-__7Ig_OjgTUNEpLuiJ2nQ"
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoiY29tcHJlbmR1cyIsImEiOiJja2Q1d2dwcmcwMGI1MnNtZ21mbjQwNjQzIn0.4pzDiBSvBwyLUhE8_hH31w";
 
 const mapContainerStyle = {
   width: "100%",
   height: "250px",
-}
+};
 
 const Map = () => {
-  const mapContainerRef = useRef(null)
+  const mapContainerRef = useRef(null);
 
-  const [map, setMap] = useState(null)
+  const [map, setMap] = useState(null);
 
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       accessToken: MAPBOX_TOKEN,
       style: "mapbox://styles/mapbox/streets-v11",
-      // Empire State Building [lng, lat]
-      center: [-73.9856, 40.7497],
-      zoom: 10,
-    })
-    map.addControl(new mapboxgl.NavigationControl(), "top-right")
+      center: [-82.9357, 42.6874],
+      zoom: 11,
+    });
+    map.addControl(new mapboxgl.NavigationControl(), "top-right");
 
-    setMap(map)
+    var marker = new mapboxgl.Marker().setLngLat([-82.9357, 42.6874]).addTo(map);
 
-    return () => map.remove()
-  }, [])
+    setMap(map);
 
-  return <div ref={mapContainerRef} style={mapContainerStyle} />
-}
+    return () => map.remove();
+  }, []);
 
-export default Map
+  return <div ref={mapContainerRef} style={mapContainerStyle} />;
+};
+
+export default Map;
