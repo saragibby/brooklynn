04f2c89c7aa2f09160a61c94d5dcb2e11e8b1b75
Sara Gibbons <saragibby@gmail.com>
2021-05-30 13:28:33 -0400

    add rsvp form

---
+++ src/components/rsvpForm.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import Airtable from "airtable";
+
+var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+  "appwdpz8rqxw74Oxy"
+);
+
+const RsvpForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [numGuests, setNumGuests] = useState("");
+  const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(true);
+
+  const saveRsvp = () => {
+    base("Table 1").create(
+      [
+        {
+          fields: {
+            name,
+            email,
+            numGuests,
+            message,
+          },
+        },
+      ],
+      function (err, records) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        records.forEach(function (record) {
+          console.log(record.getId());
+        });
+      }
+    );
+    setSubmitted(true);
+  };
+
+  return (
+    <div className="h-full">
+      {submitted && (
+        <div className="flex flex-col content-center justify-center">
+          <p className="text-2xl pb-5 text-center">Thanks for RSVPing!</p>
+          <p className="text-xl text-center">Can't wait to see you and have you with us to celebrate! 🎓</p>
+        </div>
+      )}
+      {!submitted && (
+        <form onSubmit={saveRsvp}>
+          <h3>RSVP Info</h3>
+          <fieldset>
+            <input
+              placeholder="Your name"
+              type="text"
+              tabIndex="1"
+              required
+              autoFocus
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </fieldset>
+          <fieldset>
+            <input
+              placeholder="Your Email Address"
+              type="email"
+              tabIndex="2"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </fieldset>
+          <fieldset>
+            <input
+              placeholder="Number of guests"
+              type="text"
+              tabIndex="3"
+              required
+              value={numGuests}
+              onChange={(e) => setNumGuests(e.target.value)}
+            />
+          </fieldset>
+          <fieldset>
+            <textarea
+              placeholder="Any message for the grad"
+              tabIndex="5"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+          </fieldset>
+          <fieldset>
+            <button
+              className="bg-gray-500 hover:bg-gray-800 text-white hover:text-gold font-bold py-3 px-8 rounded"
+              name="submit"
+              type="submit"
+              id="contact-submit"
+              data-submit="...Sending"
+            >
+              Submit
+            </button>
+          </fieldset>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default RsvpForm;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,61 @@
-import * as React from "react";
-import { Link } from "gatsby";
+import React, { useState } from "react";
+import RsvpForm from "../components/rsvpForm";
 import BannerSvg from "../components/bannerSvg";
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import Map from "../components/map";
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Brooklynn Gibbons" />
+const IndexPage = () => {
+  const [showRsvpForm, setShowRsvpForm] = useState(false);
 
-    <div
-      class="bg-cover bg-center w-screen h-screen"
-      style={{ backgroundImage: `url('by-river.webp')` }}
-    >
-      <div className="w-96 text-white border-2 border-white absolute right-6 top-1/3 p-4 text-center">
-        <p className="font-semibold text-2xl">Let's Celebrate</p>
-        <p className="text-xl">July 31, 2021</p>
-        <div className="p-5 relative flex justify-center">
-          <a href="#celebrate" class="arrow down" />
-        </div>
-      </div>
+  return (
+    <Layout>
+      <Seo title="Brooklynn Gibbons" />
 
-      <div className="w-screen h-28 bg-white bg-opacity-60 bottom-6 absolute flex flex-col items-center justify-center">
-        <div className="relative top-0">
-          <BannerSvg />
-          <h2 className="absolute w-full top-5 text-center text-2xl text-gold font-fancy">
-            Class of 2021
-          </h2>
-        </div>
-        <h1 className="absolute text-5xl pt-5">Brooklynn Gibbons</h1>
-      </div>
-    </div>
-    <div id="celebrate" className="p-12">
-      <div className="bg-yellow-50 rounded-xl shadow-md p-12 flex">
-        <div className="min-w-96">
-          <h3 className="text-5xl pb-5">Let's Celebrate</h3>
-          <h3 className="text-2xl pb-4">Brooklynn's Graduation Party</h3>
-          <p>July 31, 2021 | 2:00 PM – 6:00 PM</p>
-          <p>18926 Burberry Dr, Macomb, MI 48042, USA</p>
-          <div className="pt-6">
-            <button class="bg-gray-500 hover:bg-gray-800 text-white hover:text-gold font-bold py-3 px-8 rounded">
-              RSVP
-            </button>
+      <div
+        className="bg-cover bg-center w-screen h-screen"
+        style={{ backgroundImage: `url('by-river.webp')` }}
+      >
+        <div className="w-96 text-white border-2 border-white absolute right-6 top-1/3 p-4 text-center">
+          <p className="font-semibold text-2xl">Let's Celebrate</p>
+          <p className="text-xl">July 31, 2021</p>
+          <div className="p-5 relative flex justify-center">
+            <a href="#celebrate" className="arrow down" />
           </div>
         </div>
-        <div className="pl-8 w-full h-full">
-        <Map />
+
+        <div className="w-screen h-28 bg-white bg-opacity-60 bottom-6 absolute flex flex-col items-center justify-center">
+          <div className="relative top-0">
+            <BannerSvg />
+            <h2 className="absolute w-full top-5 text-center text-2xl text-gold font-fancy">
+              Class of 2021
+            </h2>
+          </div>
+          <h1 className="absolute text-5xl pt-5">Brooklynn Gibbons</h1>
         </div>
       </div>
-    </div>
-  </Layout>
-);
+      <div id="celebrate" className="p-12">
+        <div className="bg-yellow-50 rounded-xl shadow-md p-12 flex">
+          <div className="min-w-96">
+            <h3 className="text-5xl pb-5">Let's Celebrate</h3>
+            <h3 className="text-2xl pb-4">Brooklynn's Graduation Party</h3>
+            <p>July 31, 2021 | 2:00 PM – 6:00 PM</p>
+            <p>18926 Burberry Dr, Macomb, MI 48042, USA</p>
+            <div className="pt-6">
+              <button className="bg-gray-500 hover:bg-gray-800 text-white hover:text-gold font-bold py-3 px-8 rounded" onClick={() => setShowRsvpForm(!showRsvpForm)}>
+                {showRsvpForm ? "Show Map" : "RSVP" }
+              </button>
+            </div>
+          </div>
+          <div className="pl-8 w-full h-full">
+            {!showRsvpForm && <Map />}
+            {showRsvpForm && <RsvpForm />}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default IndexPage;
