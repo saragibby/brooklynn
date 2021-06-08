303085482e313200089b81a162130d0f27a18f23
Sara Gibbons <saragibby@gmail.com>
2021-06-07 22:22:58 -0400

    fix gallery overflow

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -52,8 +52,12 @@ const Gallery = () => {
     <>
       <div className="gallery grid grid-cols-4 auto-rows-14 gap-2 max-w-5xl m-auto">
         {images.map(({ id, fluid }, index) => (
-          <div onClick={(e) => handleOpen(e, index)} key={`div-${id}`}>
-            <Img key={id} fluid={fluid} className="shadow-md hover:blur-sm" />
+          <div
+            onClick={(e) => handleOpen(e, index)}
+            key={`div-${id}`}
+            className="overflow-hidden"
+          >
+            <Img key={id} fluid={fluid} />
           </div>
         ))}
       </div>
@@ -65,7 +69,10 @@ const Gallery = () => {
         selectedImage={selectedImage}
       /> */}
       {showLightbox && (
-        <div className="fixed top-0 left-0 w-screen h-screen bg-opacity-80 bg-gray-700 flex justify-center items-center" onClick={handleClose}>
+        <div
+          className="fixed top-0 left-0 w-screen h-screen bg-opacity-80 bg-gray-700 flex justify-center items-center"
+          onClick={handleClose}
+        >
           <div className="w-3/4 h-3/4">
             <Img
               fluid={images[selectedImage].fluid}
